114cc81289f7e17b14a780f93ea5e16fb2738be4
Nicolassedano23 <70270437+Nicolassedano23373@users.noreply.github.com>
2022-06-05 20:14:40 +0000

    Initial commit

---
+++ Failure.java
@@ -0,0 +1,6 @@
+public class Failure{
+  static String f = " It's not that hard to understand, choose 5 charcters ONLY! \n Are you stupid?";
+  public static void fail(){
+    System.out.println(f);
+}
+}
---
+++ Instructions.java
@@ -0,0 +1,6 @@
+public class Instructions {
+ static String m = "X means incorrect letter, ~ means correct letter in incorrect spot, ! means correct letter in correct spot. Only real words are valid.";
+  public static void instructions(){
+    System.out.println(m);
+  }
+}
